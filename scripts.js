8a5162f68733ed404954bbd00611d539c919a59c
programadormovel <iantoninatotony@gmail.com>
2024-03-14 09:12:45 -0300

    projeto inicial

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var botao = document.getElementById("nome");
+botao.addEventListener("click , alertar, false");
+
+function alertar(event){
+    if(tru){
+        alert("Voce clicou no Botão!!!" +event);
+        
+    }
+}
